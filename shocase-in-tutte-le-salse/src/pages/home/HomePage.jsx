6a35345f8902104dabbb3001c9da3eac83fd95d5
mattiCat2010 <mattiz10b@gmail.com>
2026-01-08 22:56:01 +0100

    ADD image and style the backgrownd TODO extract the backgrownd in a different component; Add the "presentation text", see figma design, in a separate component;

--- shocase-in-tutte-le-salse/src/pages/home/HomePage.jsx
+++ shocase-in-tutte-le-salse/src/pages/home/HomePage.jsx
@@ -1,5 +1,5 @@
 import { NavBar } from "../../components/NavBar";
-// import backgroundImage from "../../assets/background.png"
+import backgroundImage from "../../assets/background.png"
 import "./HomePage.css";
 
 
@@ -7,7 +7,10 @@ export function HomePage() {
     return (
         <>
             <NavBar />
-            
+            {/* fix position absolute hiding text issue, container main with a margin of 729px top? */}
+            <div class="background"> 
+                <img src={backgroundImage} alt="" />
+            </div>
         </>
     )
 }
