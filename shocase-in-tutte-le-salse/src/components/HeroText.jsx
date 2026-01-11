cb1d2049af44766e8f68ce2c44c56373e9a495b5
mattiCat2010 <mattiz10b@gmail.com>
2026-01-11 14:41:52 +0100

    REFINE HeroText component structure for improved readability

--- shocase-in-tutte-le-salse/src/components/HeroText.jsx
+++ shocase-in-tutte-le-salse/src/components/HeroText.jsx
@@ -4,9 +4,11 @@ import lineSeparator from "../assets/line-separator.png";
 export function HeroText () {
     return (
         <div className="hero-text-container">
-            <h1 className="abhaya-libre-regular" >In <strong className="abhaya-libre-bold" >Tutte</strong></h1>
-            <br></br>
-            <h1 className="abhaya-libre-regular" >Le <strong className="abhaya-libre-bold" >Salse</strong></h1>
+            <h1 className="abhaya-libre-regular">
+                In <strong className="abhaya-libre-bold">Tutte</strong>
+                <br />
+                Le <strong className="abhaya-libre-bold">Salse</strong>
+            </h1>
             <img src={lineSeparator} alt="" />
         </div>
     )
